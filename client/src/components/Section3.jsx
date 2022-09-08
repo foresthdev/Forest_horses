190f9b2c3bcb1c500c99fe0a477c8569b42e24d9
eperez045 <eperez045@gmail.com>
2022-09-08 16:02:01 +0200

    #24 estilos section 3

--- client/src/components/Section3.jsx
+++ client/src/components/Section3.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
+import foto from "../assets/mountain.jpg";
 
 export default function Section3() {
   return (
-    <div>
-    <section className='apadrina'>
+    <div className="section3-grid">
+      <section className="apadrina">
         <h2>Apadrina una hectàrea</h2>
-    </section>
-    <img src=""/>
-    <section className='donacio'></section>
+      </section>
+      <section className="img-grid">
+        <img src={foto} alt="caballo" className="img-caballo" />
+      </section>
+
+      <section className="donacio">
+        <h2>Donació</h2>
+      </section>
     </div>
-    
-  )
+  );
 }
