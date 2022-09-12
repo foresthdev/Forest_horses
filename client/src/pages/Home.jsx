21792e84fd8bed9b644dbaa95dddcd745db21595
Edurne <edurnenun@gmail.com>
2022-09-12 13:06:10 +0200

    first commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import Header from "../components/Header";
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 
 function Home() {
   return (
     <div className="Home">
-      <Header />
+      <Navbar />
+
+      <Footer />
     </div>
   );
 }
