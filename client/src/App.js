1e85e257ee7a23679574f28640bff2aa3d9e57ac
Edurne <edurnenun@gmail.com>
2022-09-13 15:58:26 +0200

    #18 components home edited

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 // import './App.css';
 import Home from './pages/Home';
-import Navbar from "./components/Navbar";
-import Footer from './components/Footer';
-import Section3 from './components/Section3';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      
       <Home />
-      <Section3 />
-      <Footer />
+      
       
     </div>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from '../components/Footer';
+import Section3 from '../components/Section3';
 
 
 function Home() {
   return (
     <div className="Home">
       <Navbar />
-
+      <Header />
+      <Section3 />
       <Footer />
     </div>
   );
