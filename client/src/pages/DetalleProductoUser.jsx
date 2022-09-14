bd21885be6d35730ca9e44eac5e8d62202a55650
Edurne <edurnenun@gmail.com>
2022-09-15 00:22:14 +0200

    #16 Render info products

--- client/src/pages/DetalleProductoUser.jsx
+++ client/src/pages/DetalleProductoUser.jsx
@@ -1,28 +1,54 @@
 import React from "react";
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import "../App.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
+import { useEffect, useState } from 'react';
 
-function Home() {
+const product = { id:1, image: "https://i.imgur.com/9NZV57d.jpg", product_name: "Tassa de ceràmica Al pas i amb calma", product_description: "Tassa de ceràmica Al pas i amb calma de mesures 8 (ample) x 9,5 cm (alt), de capacitat: 325 ml. Apta per a rentavaixella i per a microones.", product_price: "12"};
+
+function DetalleProductoUser() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const getData = new Promise(resolve => {
+      setTimeout (() => {
+        resolve(product);
+      }, 3000)
+    });
+    getData.then(res => setData(res))
+  }, [])
+
   return (
-    <div className="Home">
+    <div className="DetalleProductoUser">
       <Navbar />
-      
-      <section>
-      <div className="Header">
-        <div className="header-title" > {/*background image con parallax*/}
+      <section className="sectionDetalleUser">
+        <div className="header-title" > 
             <h1>Detall Producte</h1>
         </div>
-        <div>
-          
-        </div>
-   
-    </div>
-      </section>
 
+        <div className="detail_content">
+            <div>
+                <img className="detail_image" src={data.image} alt="" />
+                
+            </div>
+            <div detail_text>
+              <p>Producte:</p>
+              <p>{data.product}</p>
+              <p>Nom del producte:</p>
+              <p>{data.product_name}</p>
+              <p>Descripció</p>
+              <p>{data.product_description}</p>
+              <p>Preu</p>
+              <p>{data.product_price} €</p>
+              <button>Afegir al cistell</button>
+            </div>
+        </div>
+
+      </section>
       <Footer />
     </div>
   );
 }
 
-export default Home;
+export default DetalleProductoUser;
