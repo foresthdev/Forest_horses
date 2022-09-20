17c5cdc9cf867b67e29236130aef499d04bfd27c
Lori <baldomerland@gmail.com>
2022-09-20 09:15:26 +0200

    modify contain of enviament, pagament and apadrina, change order in folders

--- client/src/pages/Footer/Pagament.jsx
+++ client/src/pages/Footer/Pagament.jsx
@@ -3,22 +3,35 @@ import React from "react";
 export default function MetodesPago() {
   return (
     <div>
-    <h1>MÈTODES DE PAGAMENT</h1>
-      <p>
-        Nosaltres treballem amb: Paypal Transferencia bancaria i amb Bizum
-        Transferencia. Tots els mètodes de paganment són completament segurs.
-        Tota la seva informació personal (informació sobre pagament, direcció de
-        correu electrònic, direcció postal, etc...) i els detalls de pagament
-        estan protegits per encriptats i per un sistema d'autorització
-        adicional.
-      </p>
-      <h3>PAYPAL</h3>
-      <p>
-      Si escolleix realitzar el pagament mitjançant Paypal, es redirigirà a la plataforma de Paypal per poder completar el pagament.
-      Si no tens una comte de Paypal, pots ser creada durant el procés de compra per poder realitzar la comanda.
-      </p>
-      <h3>BIZUM</h3>
-      <p></p>
+    <Navbar />
+      <header className="Header">
+        <div className="header-title">
+          {" "}
+          {/*background image con parallax*/}
+          <h1>Botiga solidària</h1>
+        </div>
+      </header>
+      <main>
+        <h1>MÈTODES DE PAGAMENT</h1>
+        <p>
+          Nosaltres treballem amb: Paypal Transferencia bancaria i amb Bizum
+          Transferencia. Tots els mètodes de paganment són completament segurs.
+          Tota la seva informació personal (informació sobre pagament, direcció
+          de correu electrònic, direcció postal, etc...) i els detalls de
+          pagament estan protegits per encriptats i per un sistema
+          d'autorització adicional.
+        </p>
+        <h3>PAYPAL</h3>
+        <p>
+          Si escolleix realitzar el pagament mitjançant Paypal, es redirigirà a
+          la plataforma de Paypal per poder completar el pagament. Si no tens
+          una comte de Paypal, pots ser creada durant el procés de compra per
+          poder realitzar la comanda.
+        </p>
+        <h3>BIZUM</h3>
+        <p></p>
+      </main>
+      <Footer />
     </div>
   );
 }
--- client/src/pages/Nav/Apadrina.jsx
+++ client/src/pages/Nav/Apadrina.jsx
@@ -7,8 +7,35 @@ export default function Apadrina() {
   return (
     <div>
       <Navbar />
-      <Header />
-
+      <header className="Header">
+        <div className="header-title">
+          {" "}
+          {/*background image con parallax*/}
+          <h1>Apadrina</h1>
+        </div>
+      </header>
+      <main>
+        <p>
+          Sabem de la importància de cuidar els nostres boscos i la situació
+          actual de risc d'incendi ho requereix. El Forest es troba en una zona
+          de perímetre de protecció prioritària per la prevenció d'incendis
+          forestals, un territori amb un gran perill d'incendi que, a causa de
+          la continuïtat de la massa forestal, es pot convertir en un gran
+          incendi forestal. Per tal de poder obtenir els recursos necessaris per
+          seguir amb les accions de restauració forestal, prevenció d'incendis i
+          regeneració de pastura, hem engegat la campanya «Apadrina una
+          hectàrea». Tothom qui vulgui col·laborar podrà apadrinar una o més
+          hectàrees de manera simbòlica, a través d’una aportació mensual de 10
+          euros per ha. que aniran destinats a poder gestionar l'espai on
+          l'Associació actua. Amb aquesta aportació dones suport a poder
+          invertir en les tasques de restauració forestal i prevenció d'incendis
+          que es duen a terme al nostre entorn natural en les que el Forest
+          forma part. Si vols sumar-te a aquesta acció pels nostres boscos i
+          col·laborar en la neteja del bosc, omple el formulari que hi ha a
+          continuació. De part del Forest: moltes gràcies per formar part de la
+          solució!
+        </p>
+      </main>
       <Footer />
     </div>
   );
