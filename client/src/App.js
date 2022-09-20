5e4c02a1f392040279ef71d3aa8dc67c001254b0
Lori <baldomerland@gmail.com>
2022-09-20 09:02:22 +0200

    add folders to order pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 // import './App.css';
-import Home from './pages/Home';
+import Home from './pages/Nav/Home';
+import Apadrina from './pages/Nav/Apadrina';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       
       <Home />
+      <Apadrina />
       
       
     </div>
---
+++ client/src/pages/Nav/Apadrina.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+export default function Apadrina() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
