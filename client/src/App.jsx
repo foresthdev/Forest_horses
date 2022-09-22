a1990bebc811fcd57f94525ab23a5f4497816a24
alejapiatti <104015514+alejapiatti@users.noreply.github.com>
2022-09-22 09:10:43 +0200

    #1 y #17

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,31 @@
 // import './App.css';
 import Navbar from './components/Navbar';
-import Routes from './Routes';
+// import Routes from './Routes';
 import {BrowserRouter} from "react-router-dom";
 import Cataleg from './pages/tienda/Cataleg';
 import CarritoCompra from './pages/tienda/CarritoCompra';
+import Nav from './components/Navbar';
+import Home from './pages/Nav/Home';
+import Apadrina from './pages/Nav/Apadrina';
+import Registrados from '../pages/Registrados';
+import Mevesdades from '../pages/Mevesdades';
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Navbar />
+        <Nav />
         <Cataleg />
         <CarritoCompra />
+      <Home />
+      <Apadrina />
+      <Registrados />
+      <Mevesdades />
+      <Nav />
 
-        <Routes />
+        {/* <Routes /> */}
       </div>
     </BrowserRouter>
   );
