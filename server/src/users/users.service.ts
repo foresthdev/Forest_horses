62b979b5fad694668bd38d8169b18d40fbce4c74
Edurne <edurnenun@gmail.com>
2022-09-23 17:06:35 +0200

    #1 #2 #3 users.service function async addUser added with errors password and save in user no exited line 26-27

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { CreateUserDTO } from './dtos/create-user-dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -17,6 +19,12 @@ export class UsersService {
   findOne(id: number): Promise<User> {
     // In the nestjs documentation (https://docs.nestjs.com/techniques/database) say id: string pero da error
     return this.usersRepository.findOneBy({ id });
+  }
+
+  async addUser(createUserDTO): Promise<User> {
+    const newUser = await this.usersRepository.create(createUserDTO);
+    newUser.password = await bcrypt.hash(newUser.password, 10);
+    return newUser.save();
   }
 
   async remove(id: string): Promise<void> {
