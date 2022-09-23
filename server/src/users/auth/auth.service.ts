d31da19d2a3a8af7924f5c4a3aed636a23d537f9
Edurne <edurnenun@gmail.com>
2022-09-23 18:07:45 +0200

    #1 #2 #3 validateUser method added with error in line 16 and 19

--- server/src/users/auth/auth.service.ts
+++ server/src/users/auth/auth.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private jwt: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const foundUser = await this.userRepository.findOne({ username });
+    if (foundUser) {
+      if (await bcrypt.compare(password, foundUser.password)) {
+        const { password, ...result } = foundUser;
+        return result;
+      }
+
+      return null;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id, role: user.role };
+
+    return {
+      access_toen: this.jwt.sign(payload),
+    };
+  }
+}
