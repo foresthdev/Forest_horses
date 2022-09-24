52262a4135750e832507d1c1f97d73d28280666e
Edurne <edurnenun@gmail.com>
2022-09-24 14:27:59 +0200

    #1 #2 #3 auth module edited

--- server/src/users/auth/auth.module.ts
+++ server/src/users/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwStrategy } from './jwt-strategy';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user.entity';
 
 @Module({
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
 })
 export class AuthModule {}
+
+// we add the PassModule package to allow passport to handle the users’ authentication and configured jwt using the JwtModule register method. We pass in the secret we created in the constants file and specify the generated token’s expiration time
