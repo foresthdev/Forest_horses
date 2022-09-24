e6de095ccd828dad442966c5fde8dcf042031066
Edurne <edurnenun@gmail.com>
2022-09-24 14:13:06 +0200

    #1 #2 #3 jwt strategy with payload and constants files added

--- server/src/users/auth/jwt-auth.guard.ts
+++ server/src/users/auth/jwt-auth.guard.ts
@@ -3,3 +3,5 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
+
+// use the AuthGuard created in the code snippet to protect your API routes from unauthorized users.
