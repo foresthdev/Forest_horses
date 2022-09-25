3f9eecbc8e120362f8b6ac7f73835a15888df0a5
Edurne <edurnenun@gmail.com>
2022-09-25 18:00:02 +0200

    #7 cart summary added

--- client/src/pages/tienda/CarritoCompra.jsx
+++ client/src/pages/tienda/CarritoCompra.jsx
@@ -17,7 +17,7 @@ function CarritoCompra(props) {
       );
     }
   }
-
+  
   const itemsPrice = carItems.reduce((a,c) => a+c.price * c.qty, 0);
   const taxPrice = itemsPrice * 0.14;
   const shippingPrice = itemsPrice > 2000 ? 0 : 50;
@@ -48,11 +48,33 @@ function CarritoCompra(props) {
               <div>
                 {item.qty} x {item.price.toFixed(2)}
               </div>
+              <div className="block col-1 text-right">
+                {item.qty} x {item.price.toFixed}
+              </div>
             </stack>
+            
           ))}
-          <aside className="block col-1">
-
-          </aside>
+          {carItems.length !== 0 && (
+            <aside>
+              <div className="row">
+                <div className="col-2">Items Price</div>
+                <div className="col-1 text-right">{itemsPrice.toFixed(2)}</div>
+              </div>
+              <div className="row">
+                <div className="col-2">Tax Price</div>
+                <div className="col-1 text-right">{taxPrice.toFixed(2)}</div>
+              </div>
+              <div className="row">
+                <div className="col-2">Shipping Price</div>                                                            
+                <div className="col-1 text-right">{shippingPrice.toFixed(2)}</div>
+              </div>
+              <div className="row">
+                <div className="col-2">Total Price</div>
+                <div className="col-1 text-right"><strong>{totalPrice.toFixed(2)}</strong></div>
+              </div>
+            </aside>
+          )}
+          
       </main>
     
 
