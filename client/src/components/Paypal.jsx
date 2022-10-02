cd53f12bb447c4785f2dbcf287e2d825e6101f72
Lori <103057673+Lorelain90@users.noreply.github.com>
2022-10-02 13:04:54 +0200

    merge the branch#9

--- client/src/components/Paypal.jsx
+++ client/src/components/Paypal.jsx
@@ -1,1 +1,19 @@
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+const initialOptions = {
+  "client-id": "sb",
+  currency: "EUR",
+  "disable-card": "visa",
+  "disable-funding": "card",
+  intent: "capture",
+  "data-client-token": "abc123xyz==",
+};
+
+export default function App() {
+  return (
+    <PayPalScriptProvider options={{ "client-id": "test" }}>
+      <PayPalButtons style={{ layout: "horizontal", color:'gold', shape:'pill', label:'checkout', tagline:'true' }} />
+    </PayPalScriptProvider>
+  );
+}
 
--- client/src/pages/Nav/Donacions.jsx
+++ client/src/pages/Nav/Donacions.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Paypal from '../../components/Paypal';
 
 export default function Donacions() {
   return (
@@ -10,51 +11,64 @@ export default function Donacions() {
         </div>
       </header>
       <main>
-        <img
-          src="https://i.imgur.com/zjGBlyx.png"
-          className="donacions"
-          alt="donacions temps"
-        ></img>
+        <section className="donacions">
+          <img
+            src="https://i.imgur.com/lJ6w8qL.png"
+            classname="imgdonacions"
+            alt="banner donacions de temps"
+          />
+          <p>
+            Per tal de poder assolir els objectius de l'Associació, necessitem
+            persones voluntàries amants de la natura i les seves formes de vida
+            que donin suport en les tasques de restauració forestal i ens ajudin
+            a impulsar la rehabilitació del patrimoni. El suport del voluntariat
+            contribueix a la conservació dels espais naturals i en conseqüència
+            la biodiversitat, assegurant zones de pastura per als cavalls en la
+            tasca de neteja de sotabosc i prevenció d'incendis. ​ Pots unir-te
+            al Forest com a voluntari o voluntària de forma periòdica o
+            puntualment participant en les activitats que anem organitzant.
+          </p>
+        </section>
+        <section className="donacions">
+          <img
+            src="https://i.imgur.com/zCYx1FT.png"
+            classname="imgdonacions"
+            alt="banner donacions de materials"
+          />
+          <p>
+            Una altra opció de col·laborar i formar part d'aquest projecte és
+            aportant material per la tasca al Forest. Aquestes donacions són
+            també molt necessàries i ens ajuden molt. Pots donar material per: ​
+            <ul>
+              <li>Tancats</li>
+              <li> Benestar cavalls</li>
+              <li>Restauració forestal</li>
+              <li>Caixes niu</li>
+            </ul>
+             
+          </p>
+        </section>
+        <section className="donacions">
+          <img
+            src="https://i.imgur.com/zjGBlyx.png"
+            classname="imgdonacions"
+            alt="banner donacions econòmiques"
+          />
+          <p>
+            Si sents que vols col·laborar amb l'Associació però no disposes de
+            temps lliure, pots fer una aportació econòmica de forma puntal o
+            periòdica. Al ser una entitat petita, els nostres recursos econòmics
+            són mínims, per això tota ajuda és rebuda amb enorme gratitud.
+            D'aquesta forma, passes a ser part d'aquesta simbiosi, contribuint
+            al benestar dels boscos i la seva biodiversitat.
+          </p>
+          
+          <Paypal/>
+          <img src='https://i.imgur.com/Gr2FbNO.png' href='https://sis-t.redsys.es:25443/sis/rest/trataPeticionREST' alt='redirecció a redsys'/>
+        </section>
         <p>
-          Per tal de poder assolir els objectius de l'Associació, necessitem
-          persones voluntàries amants de la natura i les seves formes de vida
-          que donin suport en les tasques de restauració forestal i ens ajudin a
-          impulsar la rehabilitació del patrimoni. El suport del voluntariat
-          contribueix a la conservació dels espais naturals i en conseqüència la
-          biodiversitat, assegurant zones de pastura per als cavalls en la tasca
-          de neteja de sotabosc i prevenció d'incendis. ​ Pots unir-te al Forest
-          com a voluntari o voluntària de forma periòdica o puntualment
-          participant en les activitats que anem organitzant.
-        </p>
-        <img
-          src="https://i.imgur.com/lJ6w8qL.png"
-          className="donacions"
-          alt="donacions material"
-        ></img>
-        <p>
-          Una altra opció de col·laborar i formar part d'aquest projecte és
-          aportant material per la tasca al Forest. Aquestes donacions són també
-          molt necessàries i ens ajuden molt. Pots donar material per:
-          <ul>
-            <li>Tancats</li>
-            <li>Benestar cavalls</li>
-            <li>Restauració forestal</li>
-            <li>Caixes niu</li>
-          </ul>
-        </p>
-
-        <img
-          src="https://i.imgur.com/zCYx1FT.png"
-          className="donacions"
-          alt="donacions economica"
-        ></img>
-        <p>
-          Si sents que vols col·laborar amb l'Associació però no disposes de
-          temps lliure, pots fer una aportació econòmica de forma puntal o
-          periòdica. Al ser una entitat petita, els nostres recursos econòmics
-          són mínims, per això tota ajuda és rebuda amb enorme gratitud.
-          D'aquesta forma, passes a ser part d'aquesta simbiosi, contribuint al
-          benestar dels boscos i la seva biodiversitat.
+          I si en tens d'algún altre tipus i creus que pot servir, no dubtis en
+          escriure'ns, t'ho agrairem infinitament! :)
         </p>
       </main>
     </div>
