b343d925ab633acc34ac57b44d5852bf09eb61cd
edurnenun <edurnenun@gmail.com>
2023-02-06 12:48:44 +0100

    #7 #11 FEAT: Added localStorage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,13 +13,25 @@ import Carrito from './pages/tienda/CarritoCompra';
 import Nologin from './pages/ERROR/No';
 import Login from "./pages/login/Login";
 import Mevesdades from "./pages/login/Mevesdades";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
 function App() {
   const [cartItems, setCartItems ] = useState([]);
+  //store in localstorage
+  useEffect (() => {
+    const storedCartItems = localStorage.getItem("cartItems");
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
+  }, []);
   
+  useEffect (() => {
+    localStorage.setItem("cartItems",JSON.stringify(cartItems));
+  }, [cartItems]);
+  
+  //Add on shopping Cart
   const onAdd =(item) => {
     const existItem = cartItems.find(product => product.id === item.id); //encuentra el producto que coincida 
     if (existItem) {// si existe
