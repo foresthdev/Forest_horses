a5eb40487fa1f4e059bf14f3635d82f64545096a
edurnenun <edurnenun@gmail.com>
2023-03-20 11:29:38 +0100

    #1 #2 #2 feat(server auth):create validation Pipe and chore(server):prisma migartions

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,7 +6,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto) {
     return this.authService.signup();
   }
 
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Auto-validation ensuring all endpoints are protected from receiving incorrect data.
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
