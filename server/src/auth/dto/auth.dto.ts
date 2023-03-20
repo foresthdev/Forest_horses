8d6365a625b43fd79976bc421b6ee921efc4bf4e
edurnenun <edurnenun@gmail.com>
2023-03-20 11:46:19 +0100

    #1 #2 #2 feat(server auth): config AuthDto

---
+++ server/src/auth/dto/auth.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsEmail, IsString, Length } from 'class-validator';
+
+export class AuthDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  public email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 20, { message: 'Passord has to be at between 3 and 20 chars' })
+  public password: string;
+}
